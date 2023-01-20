31affe8a4b095136ec1a6020d710cd22ce371f36
safwaan-menk <41082275+safwaan-menk@users.noreply.github.com>
2023-01-20 15:20:43 -0600

    GTFOL PLEASSE

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,13 @@ import type { AppProps } from "next/app";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import { MantineProvider } from "@mantine/core";
 
+let uri = "http://localhost:3000/api/graphql";
+if (process.env.NODE_ENV === "production") {
+  uri = "https://socialvc.netlify.app/api/graphql";
+}
+
 const client = new ApolloClient({
-  uri: "http://localhost:3000/api/graphql",
+  uri: uri,
   cache: new InMemoryCache(),
 });
 
